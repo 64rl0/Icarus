ed20a903651c5c3f5d9330110ebc1ae715405a88
carlogtt <carlogtt@amazon.com>
2025-01-26 18:50:48 +0000

    REFACTOR: update version to 'build 2.0.74 built on 01/26/2025'

--- src/icarus/config/constants.py
+++ src/icarus/config/constants.py
@@ -56,7 +56,7 @@ CLI_DESCRIPTION = (
     ' _| \\___| \\__,_| _|    \\__,_| ____/  '
 )
 CLI_EPILOG = ''
-CLI_VERSION = 'build 2.0.73 built on 01/25/2025'
+CLI_VERSION = 'build 2.0.74 built on 01/26/2025'
 
 ROOT_DIR = pathlib.Path(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '..', '..'))
 CLI_SCRIPTS_DIR = pathlib.Path(os.path.join(ROOT_DIR, 'cli_scripts'))
